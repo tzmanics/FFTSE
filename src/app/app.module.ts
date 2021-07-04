7db500bc7e2839fca767f4e29098e6ea34806f07
Tara Z. Manicsic <tzmanics@gmail.com>
2021-07-03 23:25:12 -0400

    adds generated days component;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DaysComponent } from './days/days.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DaysComponent
   ],
   imports: [
     BrowserModule
