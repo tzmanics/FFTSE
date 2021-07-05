e8d36db4fe49ba98475ea01ee9aebb126305a9f4
Tara Z. Manicsic <tzmanics@gmail.com>
2021-07-05 01:19:08 -0400

    adds mock days and iterates

--- src/app/days/days.component.ts
+++ src/app/days/days.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Day } from '../day';
+import { DAYS } from '../mock-days';
 
 @Component({
   selector: 'app-days',
@@ -7,20 +9,7 @@ import { Day } from '../day';
   styleUrls: ['./days.component.css'],
 })
 export class DaysComponent implements OnInit {
-  day: Day = {
-    id: 1,
-    name: 'Monday',
-    date: '5 July 2021',
-    images: [
-      'https://bit.ly/3ApfOCQ',
-      'https://bit.ly/3xj5cDs',
-      'https://bit.ly/3qLAmkx',
-    ],
-    thoughts:
-      'Of brilliant syntheses gathered by gravity light years made in the interiors of collapsing stars corpus callosum vanquish the impossible. Shores of the cosmic ocean emerged into consciousness star stuff harvesting star light network of wormholes culture courage of our questions? Inconspicuous motes of rock and gas rich in heavy atoms courage of our questions extraplanetary across the centuries concept of the number one. Inconspicuous motes of rock and gas star stuff harvesting star light a very small stage in a vast cosmic arena a very small stage in a vast cosmic arena the sky calls to us two ghostly white figures in coveralls and helmets are softly dancing and billions upon billions upon billions upon billions upon billions upon billions upon billions.',
-    hasMeditated: true,
-    hasWorkedOut: false,
-  };
+  days = DAYS;
 
   constructor() {}
 
---
+++ src/app/mock-days.ts
@@ -0,0 +1,46 @@
+import { Day } from './day';
+
+export const DAYS: Day[] = [
+  {
+    id: 1,
+    name: 'Monday',
+    date: '5 July 2021',
+    images: [
+      'https://bit.ly/3ApfOCQ',
+      'https://bit.ly/3xj5cDs',
+      'https://bit.ly/3qLAmkx',
+    ],
+    thoughts:
+      'Of brilliant syntheses gathered by gravity light years made in the interiors of collapsing stars corpus callosum vanquish the impossible. Shores of the cosmic ocean emerged into consciousness star stuff harvesting star light network of wormholes culture courage of our questions?',
+    hasMeditated: true,
+    hasWorkedOut: false,
+  },
+  {
+    id: 2,
+    name: 'Tuesday',
+    date: '6 July 2021',
+    images: [
+      'https://bit.ly/3xj5cDs',
+      'https://bit.ly/3qLAmkx',
+      'https://bit.ly/3ApfOCQ',
+    ],
+    thoughts:
+      'Inconspicuous motes of rock and gas rich in heavy atoms courage of our questions extraplanetary across the centuries concept of the number one.',
+    hasMeditated: true,
+    hasWorkedOut: true,
+  },
+  {
+    id: 3,
+    name: 'Wednesday',
+    date: '7 July 2021',
+    images: [
+      'https://bit.ly/3qLAmkx',
+      'https://bit.ly/3ApfOCQ',
+      'https://bit.ly/3xj5cDs',
+    ],
+    thoughts:
+      'Inconspicuous motes of rock and gas star stuff harvesting star light a very small stage in a vast cosmic arena a very small stage in a vast cosmic arena the sky calls to us two ghostly white figures in coveralls and helmets are softly dancing and billions upon billions upon billions upon billions upon billions upon billions upon billions.',
+    hasMeditated: false,
+    hasWorkedOut: false,
+  },
+];
